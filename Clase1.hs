31c2d26fc0d6e92273870107b5fd5bb2ce601568
Martin Sogoloff <martinsogoloff@gmail.com>
2025-09-10 13:24:24 -0300

    clase 1

---
+++ Clase1.hs
@@ -0,0 +1,47 @@
+--1--
+f 1 = 8
+f 4 = 131
+f 16 = 16
+
+g x | x == 8 = 16
+    | x == 16 = 4
+    | x == 131 = 1
+    | otherwise = -1
+
+--2a
+absoluto :: Int -> Int
+absoluto n | n < 0 = -n
+           | otherwise = n
+
+--2b
+maximoAbsoluto :: Int -> Int -> Int
+maximoAbsoluto x y = max (absoluto x) (absoluto y)
+
+--2c
+maximo3 :: Int -> Int -> Int -> Int
+maximo3 a b c | a>b && a>c = a
+              | b>a && b>c = b
+              | c>a && c>b = c
+
+--2d
+algunoEsCero :: Float -> Float -> Bool
+algunoEsCero x y | x == 0 = True
+                 | y == 0 = True
+                 |otherwise = False
+
+algunoEsCero2 0 y = True
+algunoEsCero2 x 0 = True
+algunoEsCero2 x y = False
+
+--2e
+ambosSonCero :: Float -> Float -> Bool
+ambosSonCero x y | x == y == 0 = True
+ambosSonCero x y | x /== 0 = False
+ambosSonCero x y | y /== 0 = False
+
+ambosSonCero2 0 0 = True
+ambosSonCero2 x 0 = False
+ambosSonCero2 0 y = False
+
+
+
