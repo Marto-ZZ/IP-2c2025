9f691e527abe81b1b8d8aa5a8e2c1e6b1f7e309d
Marto-ZZ <martinsogoloff@gmail.com>
2025-11-09 23:54:43 -0300

    Ultimo parical, a mimir

--- Python/guias resueltas/Parcial 1c2024 t1.py
+++ Python/guias resueltas/Parcial 1c2024 t1.py
@@ -13,5 +13,130 @@ from queue import LifoQueue as Pila
 #}
 
 
-def alarma_epidemiologica(registros:list[tuple[int,str]], infeccionas:list[str], umbral: float) -> dict[str,float]:
+def contar_pacientes(registros:list[tuple[int,str]], enfermedad:str) -> int:
+    cantidad_registros = 0
+    for i in range(len(registros)):
+        if enfermedad in registros[i][1]:
+            cantidad_registros += 1
+    return cantidad_registros
+
+def alarma_epidemiologica(registros:list[tuple[int,str]], infecciosas:list[str], umbral: float) -> dict[str,float]:
+    res = {}
+
+    for enfermedad in infecciosas:
+        for i in range(len(registros)):
+            if enfermedad in registros[i][1] and enfermedad not in res:
+                porcentaje = contar_pacientes(registros, enfermedad)/len(registros)
+                if umbral < porcentaje:
+                    res[enfermedad] = porcentaje
+    return res
+
+print(alarma_epidemiologica([(101, "Gripe"),(102, "Dengue"),(103, "Gripe"),(104, "Covid"),(105, "Dengue"),(106, "Gripe"),(107, "Migraña"),(108, "Covid"),(109, "Gripe"),(110, "Dengue")], ["Gripe", "Dengue"], 0.25))
+
+
+#2) Orden de atención [1 punto]
+
+#Desde el Hospital Fernandez nos pidieron solucionar una serie de problemas relacionados con la información que maneja sobre los pacientes y el personal de salud. En #primer lugar debemos resolver en qué orden se deben atender los pacientes que llegan a la guardia. En enfermería, hay una primera instancia que clasifica en dos colas a #los pacientes: una urgente y otra postergable (esto se llama hacer triage). A partir de dichas colas que contienen la identificación del paciente, se pide devolver una #nueva cola según la siguiente especificación.
+
+#problema orden_de_atencion ( in urgentes: cola⟨Z⟩, in postergables: cola⟨Z⟩) : cola⟨Z⟩ {
+#  requiere: {no hay elementos repetidos en urgentes}
+#  requiere: {no hay elementos repetidos en postergables}
+#  requiere: {la intersección entre postergables y urgentes es vacía}
+#  requiere: {|postergables| = |urgentes|}
+#  asegura: {no hay repetidos en res }
+#  asegura: {res es permutación de la concatenación de urgentes y postergables}
+#  asegura: {Si urgentes no es vacía, en tope de res hay un elemento de urgentes}
+#  asegura: {En res no hay dos seguidos de urgentes}
+#  asegura: {En res no hay dos seguidos de postergables}
+#  asegura: {Para todo c1 y c2 de tipo "urgente" pertenecientes a urgentes si c1 aparece antes que c2 en urgentes entonces c1 aparece antes que c2 en res}
+#  asegura: {Para todo c1 y c2 de tipo "postergable" pertenecientes a postergables si c1 aparece antes que c2 en postergables entonces c1 aparece antes que c2 en res}
+#}
+
+
+def orden_de_atencion(urgentes:Cola[int], postergables:Cola[int]) -> Cola[int]:
+    res = Cola()
+    copia_urgentes = []
+    copia_postergables = []
+
+    while not urgentes.empty():
+        copia_urgentes.append(urgentes.get())
+    while not postergables.empty():
+        copia_postergables.append(postergables.get())
+
+    for i in range(len(copia_urgentes)):
+        urgentes.put(copia_urgentes[i])
+        postergables.put(copia_postergables[i])
     
+    for i in range(len(copia_urgentes)):
+        res.put(copia_urgentes[i])
+        res.put(copia_postergables[i])
+    return res       
+
+urgentes = Cola()
+urgentes.put(1)
+urgentes.put(3)
+postergables = Cola()
+postergables.put(2)
+postergables.put(4)
+
+print(orden_de_atencion(urgentes, postergables).queue)
+
+
+#3) Camas ocupadas en el hospital [2 puntos]
+
+#Queremos saber qué porcentaje de ocupación de camas hay en el hospital. El hospital se representa por una matriz en donde las filas son los pisos, y las columnas son las #camas. Los valores de la matriz son booleanos que indican si la cama está ocupada o no. Si el valor es verdadero (True) indica que la cama está ocupada. Se nos pide #programar en Python una función que devuelve una secuencia de enteros, indicando la proporción de camas ocupadas en cada piso.
+
+#problema nivel_de_ocupacion(camas_por_piso:seq⟨seq⟨Bool⟩⟩) : seq⟨R⟩ {
+#  requiere: {Todos los pisos tienen la misma cantidad de camas.}
+#  requiere: {Hay por lo menos 1 piso en el hospital.}
+#  requiere: {Hay por lo menos una cama por piso.}
+#  asegura: {|res| = |camas|}
+#  asegura: {Para todo 0<= i < |res| se cumple que res[i] es igual a la cantidad de camas ocupadas del piso i dividido el total de camas del piso i)}
+#}
+
+
+def nivel_de_ocupacion(camas_por_piso: list[list[bool]]) -> list[float]:
+    res = []
+    cantidad_camas = len(camas_por_piso[0])
+    for i in range(len(camas_por_piso)):
+        camas_ocupadas = 0
+        for j in range(cantidad_camas):
+            if camas_por_piso[i][j] == True:
+                camas_ocupadas += 1
+        res.append(camas_ocupadas/cantidad_camas)
+    return res
+
+print(nivel_de_ocupacion([[True,True,False],[False,False,False],[True,True,True]]))
+
+
+#4) Quiénes trabajaron más? (2 puntos)
+#Dado un diccionario con la cantidad de horas trabajadas por empleado, en donde la clave es el ID del empleado y el valor es una lista de las horas trabajadas por día, #queremos saber quienes trabajaron más para darles un premio. Se deberá buscar la o las claves para la cual se tiene el máximo valor de cantidad total de horas, y #devolverlas en una lista.
+
+#problema empleados_del_mes(horas:dicc⟨Z,seq⟨Z⟩⟩) : seq⟨Z⟩ {
+#  requiere: {No hay valores en horas que sean listas vacías}
+#  asegura: {Si ID pertence a res entonces ID pertence a las claves de horas}
+#  asegura: {Si ID pertenece a res, la suma de sus valores de horas es el máximo de la suma de elementos de horas de todos los otros IDs}
+#  asegura: {Para todo ID de claves de horas, si la suma de sus valores es el máximo de la suma de elementos de horas de los otros IDs, entonces ID pertences a res}
+#}
+
+
+def sumar_horas(horas_empleado:list[int]) -> int:
+    total = 0
+    for i in range(len(horas_empleado)):
+        total += horas_empleado[i]
+    return total
+
+def empleados_del_mes(horas:dict[int,list[int]]) -> list[int]:
+    res = []
+    horas_max = [0]
+    for i in horas.keys():
+        if horas_max[0] < sumar_horas(horas[i]):
+            horas_max.clear()
+            horas_max.append(sumar_horas(horas[i]))
+            res.clear()
+            res.append(i)
+        elif horas_max[0] == sumar_horas(horas[i]):
+            res.append(i)
+    return res
+
+print(empleados_del_mes({101: [8, 10, 8], 102: [5, 5, 5], 103: [10, 8, 9], 104: [9, 9, 9]}))
